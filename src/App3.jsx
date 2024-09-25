075d087e326eb4ea4dc99a769644ebf55cc3ec28
mkamaran24 <mhamad.00192801@gmail.com>
2024-09-25 23:11:18 +0300

    form-submit-done

--- src/App3.jsx
+++ src/App3.jsx
@@ -7,8 +7,11 @@ function App3() {
     email: "",
     comments: "",
     isFreindly: true,
+    employment: "",
+    favColor: "",
   });
-  console.log(formData);
+
+  const id = React.useId();
 
   function handleChange(event) {
     const { name, value, type, checked } = event.target;
@@ -19,29 +22,38 @@ function App3() {
       };
     });
   }
+
+  function handleForm(event) {
+    event.preventDefault();
+    console.log("form submited successfully!");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleForm}>
       <h1>Test FORM React</h1>
+      <label htmlFor={id + "-firstname"}>First Name</label>
       <input
         type="text"
-        placeholder="First Name"
         onChange={handleChange}
         name="firstName"
         value={formData.firstName}
+        id={id + "-firstName"}
       />
+      <label htmlFor={id + "-lastname"}>Last Name</label>
       <input
         type="text"
-        placeholder="Last Name"
         onChange={handleChange}
         name="lastName"
         value={formData.lastName}
+        id={id + "-lastname"}
       />
+      <label htmlFor={id + "-email"}>Email</label>
       <input
         type="email"
-        placeholder="Email"
         onChange={handleChange}
         name="email"
         value={formData.email}
+        id={id + "-email"}
       />
       <textarea
         value={formData.comments}
@@ -58,6 +70,61 @@ function App3() {
       />
       <label htmlFor="isFriendly">Are you friendly?</label>
       <br />
+      <br />
+      <fieldset>
+        <legend>Current employment status</legend>
+
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          checked={formData.employment === "unemployed"}
+          onChange={handleChange}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+        <br />
+
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          checked={formData.employment === "part-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="part-time">Part-time</label>
+        <br />
+
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          checked={formData.employment === "full-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="full-time">Full-time</label>
+        <br />
+      </fieldset>
+      <br />
+      <select
+        id="favColor"
+        value={formData.favColor}
+        onChange={handleChange}
+        name="favColor"
+      >
+        <option value="">-- Choose --</option>
+        <option value="red">Red</option>
+        <option value="orange">Orange</option>
+        <option value="yellow">Yellow</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+        <option value="indigo">Indigo</option>
+        <option value="violet">Violet</option>
+      </select>
+      <br />
+      <button>Submit!</button>
     </form>
   );
 }
