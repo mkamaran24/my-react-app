1ddc1f5456f3a34d5a92fd8944ef5ae5ae7005df
mkamaran24 <mhamad.00192801@gmail.com>
2024-11-29 14:03:09 -0800

    Tic-Toe-Done

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Square(props) {
   return (
-    <button onClick={props.hold} className="seq">
+    <button onClick={props.hold} className="seq" disabled={props.lock}>
       {props.value}
     </button>
   );
@@ -12,17 +12,70 @@ function Board() {
   let [val, setVal] = React.useState(() => generateValArray());
   let oldVal = React.useRef(null);
   let [playyerTurn, setPlayerTurn] = React.useState("X");
+  let [winGame, setWinGame] = React.useState(false);
 
   function generateValArray() {
     return new Array(9).fill(null);
   }
 
-  console.log("renders");
+  function checkWinner(val) {
+    if (isX(val) || isO(val)) {
+      setWinGame(true);
+    }
+  }
+
+  function isX(val) {
+    if (val[0] === "X" && val[1] === "X" && val[2] === "X") {
+      return true;
+    } else if (val[3] === "X" && val[4] === "X" && val[5] === "X") {
+      return true;
+    } else if (val[6] === "X" && val[7] === "X" && val[8] === "X") {
+      return true;
+    } else if (val[0] === "X" && val[3] === "X" && val[6] === "X") {
+      return true;
+    } else if (val[1] === "X" && val[4] === "X" && val[7] === "X") {
+      return true;
+    } else if (val[2] === "X" && val[5] === "X" && val[8] === "X") {
+      return true;
+    } else if (val[0] === "X" && val[4] === "X" && val[8] === "X") {
+      return true;
+    } else if (val[2] === "X" && val[4] === "X" && val[6] === "X") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  function isO(val) {
+    if (val[0] === "O" && val[1] === "O" && val[2] === "O") {
+      return true;
+    } else if (val[3] === "O" && val[4] === "O" && val[5] === "O") {
+      return true;
+    } else if (val[6] === "O" && val[7] === "O" && val[8] === "O") {
+      return true;
+    } else if (val[0] === "O" && val[3] === "O" && val[6] === "O") {
+      return true;
+    } else if (val[1] === "O" && val[4] === "O" && val[7] === "O") {
+      return true;
+    } else if (val[2] === "O" && val[5] === "O" && val[8] === "O") {
+      return true;
+    } else if (val[0] === "O" && val[4] === "O" && val[8] === "O") {
+      return true;
+    } else if (val[2] === "O" && val[4] === "O" && val[6] === "O") {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   const btnArray = new Array(9)
     .fill(0)
     .map((_, index) => (
-      <Square key={index} hold={() => putValEvent(index)} value={val[index]} />
+      <Square
+        key={index}
+        hold={() => putValEvent(index)}
+        value={val[index]}
+        lock={winGame}
+      />
     ));
 
   // Handle click events
@@ -46,12 +99,20 @@ function Board() {
         setPlayerTurn("O");
       }
     }
+    checkWinner(newVal);
+  }
+
+  function resetGame() {
+    setWinGame(false);
+    setVal(generateValArray());
   }
 
   return (
     <>
       <h1>Player Turn: {playyerTurn}</h1>
       <div className="board">{btnArray}</div>
+      {winGame && <h1>Player {oldVal.current} win the game</h1>}
+      {winGame && <button onClick={resetGame}>Restart Game</button>}
     </>
   );
 }
